2692b43894bd2f5282384302030a548a4ff97423
kushal <kushaldeveloper@gmail.com>
2015-06-22 01:48:00 -0500

    create a proof of concept console application

---
+++ HappyCows.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyCows.Console
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            const string apiKey = "";
+            using (var client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                const string urlPrefix = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/";
+                const string roster = "by-name";
+                const string myName = "kkus";
+                string apiKeySuffix = "?api_key=" + apiKey;
+                var myJson = client.DownloadString(urlPrefix + roster + "/" + myName + apiKeySuffix);
+                System.Console.Write(myJson);
+                System.Console.Read();
+            }
+        }
+    }
+}
