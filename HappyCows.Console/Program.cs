ac5cb3cbdd16c64b41330ba9b9683f189b5d5421
kushal <kushaldeveloper@gmail.com>
2015-06-22 02:06:57 -0500

    extract get summoner information by name to its own method

--- HappyCows.Console/Program.cs
+++ HappyCows.Console/Program.cs
@@ -11,14 +11,19 @@ namespace HappyCows.Console
             using (var client = new WebClient())
             {
                 client.Encoding = Encoding.UTF8;
-                const string urlPrefix = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/";
-                const string roster = "by-name";
-                const string myName = "kkus";
-                string apiKeySuffix = "?api_key=" + apiKey;
-                var myJson = client.DownloadString(urlPrefix + roster + "/" + myName + apiKeySuffix);
-                System.Console.Write(myJson);
+                var summonerInformation = GetSummonerInformationByName(client: client, apiKey: apiKey, summonerName: "kkus");
+                System.Console.WriteLine(summonerInformation);
                 System.Console.Read();
             }
+        }
+
+        private static string GetSummonerInformationByName(WebClient client, string apiKey, string summonerName)
+        {
+            const string urlPrefix = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/";
+            const string roster = "by-name";
+            string apiKeySuffix = "?api_key=" + apiKey;
+            var returnString = client.DownloadString(urlPrefix + roster + "/" + summonerName + apiKeySuffix);
+            return returnString;
         }
     }
 }
